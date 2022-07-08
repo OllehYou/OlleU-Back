ca2a7d104da90edd3254ed9e5b878a0ffc0d2532
yein-lee <agop0310@gmail.com>
2022-07-08 18:06:50 +0900

    feat: 스프링 세팅

---
+++ src/main/java/com/example/OlleUBack/OlleUBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.OlleUBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlleUBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlleUBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OlleUBack/OlleUBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OlleUBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlleUBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
